f72f11dba59fa2f4e515115eb3b1325b7a5611da
taKKen <99165619+takgeunOh@users.noreply.github.com>
2022-03-03 23:52:39 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i, j;
+	int N;
+
+	cin >> N;
+
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j <= i; j++)
+			cout << "*";
+		cout << "\n";
+	}
+}
