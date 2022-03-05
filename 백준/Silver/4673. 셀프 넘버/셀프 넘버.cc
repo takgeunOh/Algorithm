7e1ca33693b8391b1561298d359d598d226a997a
taKKen <99165619+takgeunOh@users.noreply.github.com>
2022-03-06 00:47:59 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int d(int n);
+
+int main()
+{
+	int n=1;
+	int temp[10003] = { 0, };
+	int result;
+	
+	while (1)
+	{
+		result = d(n);
+		if (result>10002)
+			break;
+
+		// cout << d(n) << '\n'; 
+		// 셀프넘버가 아닌 숫자 출력 확인
+		// 이 숫자들을 인덱스로 해서 셀프넘버가 아닌 숫자인 인덱스에는 1값을 주기로 하자.
+
+		temp[result] = 1;
+
+		n++;
+	}
+
+	for (int i = 1; i < 10001; i++)
+	{
+		if (temp[i] == 0)
+			cout << i << '\n';
+	}
+}
+
+int d(int n)
+{
+	int sum = 0;
+	int temp = n;
+
+	while (1)
+	{
+		if (temp == 0)
+			break;
+		sum = sum + (temp % 10);
+		temp = temp / 10;
+	}
+
+	return n + sum;
+}
