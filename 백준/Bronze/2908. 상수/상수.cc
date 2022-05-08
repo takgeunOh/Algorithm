0c604bf3788ca73de02378363665e70a2db8b5d2
taKKen <99165619+takgeunOh@users.noreply.github.com>
2022-05-08 23:34:42 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.cc
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -1,37 +1,35 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <set>
 using namespace std;
 
-/*
-    734 893이 입력되면 437 398 중 큰 수 출력
-    두 수는 같지 않은 세 자리 수
-    0이 포함되어 있지 않다
-*/
+int main() {
+	int a, b;
+	int i;
+	int ra = 0 , rb = 0;
+	int tempa, tempb;
+	int r = 100;
 
-int main()
-{
-    int A, B;
-    int temp_a[3] = { 0, }, temp_b[3] = { 0, };
-    int r = 1;
-    int i;
-    int result_a, result_b;
+	cin >> a >> b;
 
-    cin >> A >> B;
+	for (i = 0; i < 3; i++) {
+		tempa = a % 10;
+		tempb = b % 10;
 
-    for (i = 0;i<3; i++)
-    {
-        temp_a[i] = A / r % 10;
-        temp_b[i] = B / r % 10;
+		ra = ra + tempa * r;
+		rb = rb + tempb * r;
 
-        r = r * 10;
-    }
+		r = r / 10;
+		a = a / 10;
+		b = b / 10;
+	}
 
-    result_a = temp_a[0] * 100 + temp_a[1] * 10 + temp_a[2];
-    result_b = temp_b[0] * 100 + temp_b[1] * 10 + temp_b[2];
+	// cout << ra << ", " << rb;
 
-    if (result_a > result_b)
-        cout << result_a;
-    else
-        cout << result_b;
+	if (ra > rb)
+		cout << ra;
+	else
+		cout << rb;
 }
