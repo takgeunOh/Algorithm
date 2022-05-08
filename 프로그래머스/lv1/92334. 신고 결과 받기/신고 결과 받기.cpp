afbe9a01cae057c49b73946454b2f1cfde697f08
taKKen <99165619+takgeunOh@users.noreply.github.com>
2022-05-09 00:28:32 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -1,40 +1,44 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 #include <vector>
 #include <map>
 #include <set>
+
 using namespace std;
 
-map<string, int> reportCnt;		// 유저별 신고당한 횟수
-map<string, set<string>> reportLog;	// 유저별 신고한 타 유저 리스트
+// 유저별 신고한 타 유저 리스트
+map<string, set<string>> reportLog;
+// 유저별 신고당한 횟수
+map<string, int> reportCnt;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-	vector<int> answer;
+    vector<int> answer;
 
 	for (string s : report) {
-		// 문자열 파싱
-		int blank = s.find(' ');
-		string from = s.substr(0, blank);
-		string to = s.substr(blank);
+		int blank_idx = s.find(' ');
+		string from = s.substr(0, blank_idx);
+		string to = s.substr(blank_idx);
 
-		// from -> to 를 신고한 이력이 없다면 반영한다.
+		// cout << from << ' ' << to << '\n';
+
 		if (reportLog[from].find(to) == reportLog[from].end()) {
-			reportCnt[to]++;
+			// 리턴값이 v.end()인 경우 해당 원소가 존재하지 않는 것.
+			// 즉, from이 to를 신고한 이력이 없는 경우
+			reportCnt[to]++;		// to가 신고당한 횟수
 			reportLog[from].insert(to);
 		}
 	}
 
 	for (string s : id_list) {
-		int res = 0;
+		int count = 0;
 
-		// k번 이상 신고 당했따면 결과 메일 +1
 		for (string str : reportLog[s]) {
 			if (reportCnt[str] >= k)
-				res++;
+				count++;
 		}
 
-		answer.push_back(res);
+		answer.push_back(count);
 	}
-
-	return answer;
+    
+    return answer;
 }
